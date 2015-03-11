152b9d6515e13eb2e72bba82c4621f0122e07a52
Audric de Schaetzen <audric.deschaetzen+github@gmail.com>
2015-03-11 11:51:02 +0100

    MD5Hash functionality

---
+++ Crypto/Crypto/MD5Hash.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Crypto
+{
+	public static class MD5Hash
+	{
+		public static byte[] Encrypt(byte[] source)
+		{
+			using (MD5 md5 = MD5.Create())
+			{
+				return md5.ComputeHash(source);
+			}
+		}
+
+		public static byte[] Encrypt(string path)
+		{
+			using (MD5 md5 = MD5.Create())
+			{
+				using (FileStream fstream = File.OpenRead(path))
+				{
+					return md5.ComputeHash(fstream);
+				}
+			}
+		}
+	}
+}
