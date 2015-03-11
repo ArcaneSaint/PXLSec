3810064f655a94a2de162fa8b5b38aea8190ddde
Audric de Schaetzen <audric.deschaetzen+github@gmail.com>
2015-03-11 11:59:29 +0100

    Added documentation

--- Crypto/Crypto/MD5Hash.cs
+++ Crypto/Crypto/MD5Hash.cs
@@ -10,6 +10,11 @@ namespace Crypto
 {
 	public static class MD5Hash
 	{
+		/// <summary>
+		/// Computes the MD5 hash of a given array of data.
+		/// </summary>
+		/// <param name="source">Data to be hashed.</param>
+		/// <returns>Hash, heroin and other drugs.</returns>
 		public static byte[] Encrypt(byte[] source)
 		{
 			using (MD5 md5 = MD5.Create())
@@ -17,7 +22,11 @@ namespace Crypto
 				return md5.ComputeHash(source);
 			}
 		}
-
+		/// <summary>
+		/// Computes the MD5 hash of a given file.
+		/// </summary>
+		/// <param name="path">Path of the file.</param>
+		/// <returns>the file's MD5 hash.</returns>
 		public static byte[] Encrypt(string path)
 		{
 			using (MD5 md5 = MD5.Create())
