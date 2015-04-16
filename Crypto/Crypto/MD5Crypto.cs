49519fe6b1cd9428ba41c9bbc36f4081e3ab2448
Audric de Schaetzen <audric.deschaetzen+github@gmail.com>
2015-04-16 19:21:36 +0200

    MD5 + start console test app

---
+++ Crypto/Crypto/MD5Crypto.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Crypto
+{
+	public class MD5Crypto
+	{
+
+
+		public string GetHash(string source)
+		{
+			byte[] result = null;
+			using (var md5 = MD5.Create())
+			{
+				byte[] sourceBytes = Encoding.UTF8.GetBytes(source);
+				result = md5.ComputeHash(sourceBytes);
+			}
+			if (result != null)
+			{
+				return BitConverter.ToString(result).Replace("-", string.Empty);
+			}
+			else
+				throw new ArgumentException();
+		}
+
+		public string GetFileChecksum(string fileName)
+		{
+			byte[] result = null;
+			using (var md5 = MD5.Create())
+			{
+				using (var stream = File.OpenRead(fileName))
+				{
+					 result = md5.ComputeHash(stream);
+				}
+			}
+			if (result != null)
+			{
+				return BitConverter.ToString(result).Replace("-", string.Empty);
+			}
+			else
+				throw new ArgumentException();
+		}
+	}
+}
---
+++ Crypto/CryptoUnitTests/MD5CryptoTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Crypto;
+
+namespace CryptoUnitTests
+{
+	[TestClass]
+	public class MD5CryptoTest
+	{
+		[TestMethod]
+		public void MD5Create()
+		{
+			MD5Crypto md5 = new MD5Crypto();
+			Assert.IsNotNull(md5);
+		}
+
+		[TestMethod]
+		public void MD5GetStringHash()
+		{
+			MD5Crypto md5 = new MD5Crypto();
+			string result = null;
+			result = md5.GetHash("Hello world");
+
+			Assert.IsNotNull(md5);
+		}
+
+
+	}
+}
